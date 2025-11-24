1e5d02d9c284d555316f16da2d24aedb714feab1
max <40187297+maxbarkouras@users.noreply.github.com>
2025-11-24 10:24:12 -0800

    Update firmware-createAP.c

--- firmware-createAP.c
+++ firmware-createAP.c
@@ -1,16 +1,22 @@
+// include ESP32 driver headers for wifi/webserver and CAN managment
 #include <WiFi.h>
 #include <WebServer.h>
 #include <string.h>
 #include <stdlib.h>
 #include "driver/twai.h"
 
+// define constants
+// AP SSID and password, web server info
 const char* ap_ssid = "tinCAN";
 const char* ap_pass = "CHANGE ME";
 WebServer server(80);
+
+// hardware definitions; GPIO & LED 
 const int LED = 2;
 static const gpio_num_t CAN_TX_PIN = GPIO_NUM_21;
 static const gpio_num_t CAN_RX_PIN = GPIO_NUM_22;
 
+//web portal html
 const char* page =
 "<!doctype html><html><body>"
 "<h2>CAN CONTROL</h2>"
@@ -20,11 +26,14 @@ const char* page =
 "<input type='text' name='msg', value='FFFFFFFFFFFFFFFF', maxlength='16', size='17'>"
 "<button>Send</button></form></body></html>";
 
+// setup web server
 void setup_server() {
   server.send(200, "text/html", page);
 }
 
-void get_data() {
+// recieve data from web server input and send
+void send_msg() {
+  // blink LED and strip web data into vars
   pinMode(LED, OUTPUT);
   twai_message_t m = {};
   m.extd = 0;
@@ -38,6 +47,8 @@ void get_data() {
       char current_byte[3] = {(server.arg("msg"))[i*2], (server.arg("msg"))[i*2+1], '\0'};
       m.data[i] = strtol(current_byte, NULL, 16);
     }
+
+    // send defined CAN message over gpio & blink LED
     if (twai_transmit(&m, pdMS_TO_TICKS(100)) == ESP_OK ) {
       Serial.println("sent");
       digitalWrite(LED, HIGH);
@@ -47,17 +58,23 @@ void get_data() {
       Serial.println("failed");
     }
   }
+
+  // send user back to home page 
   server.sendHeader("Location", "/");
   server.send(303);
 }
 
+// listen for messages sent on the CAN line and output
 void listen(void*) {
   ;
 }
 
 void setup() {
+
+  // set baud rate
   Serial.begin(115200);
 
+  // config ESP CAN drivers
   twai_general_config_t g = TWAI_GENERAL_CONFIG_DEFAULT(CAN_TX_PIN, CAN_RX_PIN, TWAI_MODE_NORMAL);
   twai_timing_config_t t = TWAI_TIMING_CONFIG_500KBITS();
   twai_filter_config_t f = TWAI_FILTER_CONFIG_ACCEPT_ALL();
@@ -72,6 +89,7 @@ void setup() {
   }
   Serial.println("CAN up 500k");
 
+  // create access point & start web server 
   WiFi.mode(WIFI_AP);
   IPAddress local_IP(192, 168, 1, 1);
   IPAddress gateway (192, 168, 1, 1);
@@ -84,12 +102,14 @@ void setup() {
     Serial.println("AP start failed");
     while (true) { delay(1000); }
   }
+  
   delay(100); 
   server.on("/", setup_server);
-  server.on("/set", HTTP_POST, get_data);
+  server.on("/set", HTTP_POST, send_msg);
   server.begin();
 }
 
+// continuously handle http connections
 void loop() {
   server.handleClient();
 }
